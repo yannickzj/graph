040fcc6a908cf248e7345638438bee7ca853df59
yannick <yannickzj@gmail.com>
2016-11-10 16:20:40 -0500

    implement some class methods

--- graph.h
+++ graph.h
@@ -1,25 +1,138 @@
 #pragma once
 #include <iostream>
+#include <fstream>
 #include <string>
 #include <vector>
 #include <map>
 #include "vertex_edge.h"
+
+#define BUFFERSIZE 256
+#define VERTEX_STR "#vertex"
+#define EDGE_STR "#edge"
+#define PATH_STR "#path"
+#define END_STR "#path"
+
 using namespace std;
 
 class Graph {
 private:
 	string name;
 
-	map<Vertex, vector<Edge>> adj_list;
+	map<string, vector<string> > adjList;
+
+	map<string, Vertex> vertexMap;
+
+	map<string, Edge> edgeMap;
 
 	static int numVertex;
 
 	static int numEdge;
 
+	vector<string> split(string str, string pattern) {
+		
+		string::size_type start = 0;
+		string::size_type end;
+		vector<string> result;
+		str += pattern;
+		int size = str.size();
+
+		while (start < size) {
+			end = str.find(pattern, start);
+			if (end > start) {
+				string substr = str.substr(start, end - start);
+				result.push_back(substr);
+			}
+			start = end + pattern.size();
+		}
+
+		return result;
+	}
+
+	void strPrint(vector<string> strList) {
+		if (strList.size() == 0) {
+			return;
+		}
+		cout << "(";
+		for (int i = 0; i < strList.size() - 1; i++) {
+			cout << strList[i] << ",";
+		}
+		cout << strList[strList.size() - 1] << ")" << endl;
+	}
+
+	void retrieveVertex(string str) {
+		cout << str << endl;
+		vector<string> strSplit = split(str, " ");
+		string name = strSplit[0];
+		int typeInt = atoi(strSplit[1].c_str());
+		double x = atof(strSplit[2].c_str());
+		double y = atof(strSplit[3].c_str());
+
+		vertexType type;
+		if (typeInt == 0 || typeInt == 1) {
+			type = (vertexType)typeInt;
+		}
+		else {
+			cout << "vertexType does not match in \"" << str << "\"!" << endl;
+		}
+		
+		cout << "name = " << name << "; type = " << type << "; x = " << x << "; y = " << y << endl;
+		addVertex(name, type, x, y);
+	}
+
+	void retrieveEdge(string str) {
+		cout << str << endl;
+		vector<string> strSplit = split(str, " ");
+		string name = strSplit[0];
+		string v1 = strSplit[1];
+		string v2 = strSplit[2];
+		int dirInt = atoi(strSplit[3].c_str());
+		int speed = atoi(strSplit[4].c_str());
+		double length = atof(strSplit[5].c_str());
+		int eventTypeInt = atoi(strSplit[6].c_str());
+
+		dirType dir;
+		if (dirInt == 0 || dirInt == 1 || dirInt == 2) {
+			dir = (dirType)dirInt;
+		}
+		else {
+			cout << "dirType does not match in \"" << str << "\"!" << endl;
+		}
+
+		eventType type;
+		if (eventTypeInt == 0 || eventTypeInt == 1) {
+			type = (eventType) eventTypeInt;
+		}
+		else {
+			cout << "eventType does not match in \"" << str << "\"!" << endl;
+		}
+
+		cout << "name = " << name << "; v1 = " << v1 << "; v2 = " << v2 << "; dir = " << dir << "; speed = " << speed << "; length = " << length << "; type = " << type << endl;
+		addEdge(name, v1, v2, dir, speed, length, type);
+	}
+
+	void retrievePath(string str) {
+		cout << str << endl;
+		vector<string> strSplit = split(str, " ");
+		string name = strSplit[0];
+		vector<string> edges;
+		for (int i = 1; i < strSplit.size(); i++) {
+			edges.push_back(strSplit[i]);
+		}
+
+		cout << "name = " << name;
+		for (int i = 0; i < edges.size(); i++) {
+			cout << "; edge" << i << " = " << edges[i];
+		}
+
+		cout << endl;
+
+		//road(name, edges);
+	}
+
 public:
 	Graph() {}
 
-	Graph(string _name): name(_name), adj_list() {}
+	Graph(string _name) : name(_name) {}
 
 	~Graph() {}
 
@@ -27,26 +140,178 @@ public:
 			return name;
 	}
 
-	void addVertex(vertex_type type, string label) {}
+	Vertex* getVertex(string name) {
+		if (containsVertex(name)) {
+			return &vertexMap.at(name);
+		}
+		else {
+			cout << "The graph does not contain the vertex \"" << name << "\"!" << endl;
+			exit(1);
+		}
+	}
 
-	void addEdge(Vertex v1, Vertex v2, direction dir, int speed, double length) {}
+	Edge* getEdge(string name) {
+		if (containsEdge(name)) {
+			return &edgeMap.at(name);
+		}
+		else {
+			cout << "The graph does not contain the edge \"" << name << "\"!" << endl;
+			exit(1);
+		}
+	}
 
-	void edgeEvent(Edge edge, edge_event_type eventType) {}
+	void addVertex(string label, vertexType type, double x, double y)
+	{
+		Vertex newVertex(label, type, x, y);
+		vertexMap.insert(make_pair(label, newVertex));
+		vector <string> newList;
+		adjList.insert(make_pair(label, newList));
+		numVertex++;
+	}
+
+	void addEdge(string label, string v1, string v2, dirType dir, int speed, double length, eventType type)
+	{
+		if (!containsVertex(v1) || !containsVertex(v2)) {
+			cout << "error: edge vertex does not exist!" << endl;
+			exit(1);
+		}
+
+		Edge newEdge(v1, v2, dir, speed, length, type);
+		edgeMap.insert(make_pair(label, newEdge));
+
+		if (dir == V1_TO_V2)
+			adjList[v1].push_back(label);
+		if (dir == V2_TO_V1)
+			adjList[v2].push_back(label);
+		if (dir == BI_DIRECTIONAL)
+		{
+			adjList[v1].push_back(label);
+			adjList[v2].push_back(label);
+		}
+		numEdge++;
+	}
+
+	void edgeEvent(string label, eventType type)
+	{
+		Edge* e = getEdge(label);
+		e->setEventType(type);
+	}
 
 	Path road(Edge edges[], string name) {}
 
 	Path trip(Vertex fromVertex, Vertex toVertex) {}
 
-	bool vertex(string label) {}
+	bool vertex(string label)
+	{
+		std::map<string, vector<string> >::iterator it1;
+		it1 = adjList.find(label);
+		if (it1 != adjList.end())
+		{
+			return true;
+		}
+		return false;
+	}
+
+	bool containsVertex(string label)
+	{
+		map<string, Vertex>::iterator it1;
+		it1 = vertexMap.find(label);
+		if (it1 != vertexMap.end())
+		{
+			return true;
+		}
+		return false;
+	}
+
+	bool containsEdge(string label)
+	{
+		map<string, Edge>::iterator it1;
+		it1 = edgeMap.find(label);
+		if (it1 != edgeMap.end())
+		{
+			return true;
+		}
+		return false;
+	}
 
 	void store(string filename) {}
 
-	void retrieve(string filename) {}
+	void retrieve(string filename) {
+		ifstream infile;
+		infile.open(filename);
+
+		if (!infile.good()) {
+			cout << "cannot open the input file!" << endl;
+		}
+
+		char* buffer = new char[BUFFERSIZE];
+		infile.getline(buffer, BUFFERSIZE);
+		string s = buffer;
+
+		while (!infile.eof() && s.find(END_STR) == string::npos) {
+
+			if (s.find(VERTEX_STR) != string::npos) {
+				infile.getline(buffer, BUFFERSIZE);
+				s = buffer;
+
+				while (s.find("#") == string::npos && !infile.eof()) {
+					if (!s.empty()) {
+						retrieveVertex(s);
+					}
+					infile.getline(buffer, BUFFERSIZE);
+					s = buffer;
+				}
+				cout << endl;
+			}
+
+			if (s.find(EDGE_STR) != string::npos) {
+				infile.getline(buffer, BUFFERSIZE);
+				s = buffer;
+
+				while (s.find("#") == string::npos && !infile.eof()) {
+					if (!s.empty()) {
+						retrieveEdge(s);
+					}
+					infile.getline(buffer, BUFFERSIZE);
+					s = buffer;
+				}
+				cout << endl;
+			}
+
+			if (s.find(PATH_STR) != string::npos) {
+				infile.getline(buffer, BUFFERSIZE);
+				s = buffer;
+
+				while (s.find("#") == string::npos && !infile.eof()) {
+					if (!s.empty()) {
+						retrievePath(s);
+					}
+					infile.getline(buffer, BUFFERSIZE);
+					s = buffer;
+				}
+				cout << endl;
+			}
+
+			infile.getline(buffer, BUFFERSIZE);
+			s = buffer;
+				
+		}
+
+		delete buffer;
+
+		infile.close();
+	}
 		
+	static int getNumVertex() {
+		return numVertex;
+	}
+
+	static int getNumEdge() {
+		return numEdge;
+	}
+
 };
 
 int Graph::numEdge = 0;
 
 int Graph::numVertex = 0;
-
-
--- vertex_edge.h
+++ vertex_edge.h
@@ -1,53 +1,66 @@
 #include <iostream>
 #include <string>
 #include <vector>
+
 using namespace std;
 
-enum vertex_type {POINT_OF_INTEREST = 0, INTERSECTION = 1};
-enum direction {BI_DIRECTIONAL = 0, V1_TO_V2 = 1, V2_TO_V1 = 2};
-enum edge_event_type{OPEN = 0, CLOSE = 1};
+enum vertexType { POINT_OF_INTEREST = 0, INTERSECTION = 1 };
+enum dirType { BI_DIRECTIONAL = 0, V1_TO_V2 = 1, V2_TO_V1 = 2 };
+enum eventType { OPEN = 0, CLOSE = 1 };
 
 class Vertex
 {
 
 private:
-        string name;
+	string name;
 
-        vertex_type type;
+	vertexType type;
 
+	double x;
+
+	double y;
 
 public:
-        double x;
 
-        double y;
 
-        Vertex(string _name = "unnamed" , vertex_type _type = POINT_OF_INTEREST):name(_name),type(_type){}
+	//Vertex(string _name = "unnamed", vertexType _type = POINT_OF_INTEREST) :name(_name), type(_type) {}
 
-        ~Vertex(){}
+	Vertex(string _name = "unnamed", vertexType _type = POINT_OF_INTEREST, double _x = 0, double _y = 0) :name(_name), type(_type), x(_x), y(_y) {}
 
-        Vertex(const Vertex& p)  // copy constructor
-        {
-            *this = p;
-        }
+	~Vertex() {}
 
-        Vertex & operator = (const Vertex& p)    //operator =
-        {
-            if(this == &p) return(*this);
-            name = p.name;
-            type = p.type;
-            return (*this);
-        }
+	//Vertex(const Vertex& p)  // copy constructor
+	//{
+	//	*this = p;
+	//}
 
-        string getName(void)
-        {
-            return name;
-        }
+	//Vertex & operator = (const Vertex& p)    //operator =
+	//{
+	//	if (this == &p) return(*this);
+	//	name = p.name;
+	//	type = p.type;
+	//	x = p.x;
+	//	y = p.y;
+	//	return (*this);
+	//}
 
-        vertex_type getType(void)
-        {
-            return type;
-        }
+	string getName()
+	{
+		return name;
+	}
 
+	vertexType getType()
+	{
+		return type;
+	}
+
+	void setType(vertexType _type) {
+		type = _type;
+	}
+
+	void print() {
+		cout << "vertex: name = " << name << ", type = " << type << ", x = " << x << ", y = " << y << endl;
+	}
 
 
 };
@@ -55,86 +68,89 @@ public:
 class Edge
 {
 private:
-        Vertex v1;
+	string v1;
 
-        Vertex v2;
+	string v2;
 
-        direction dir;
+	dirType dir;
 
-        int speed;
+	int speed;
 
-        double length;
+	double length;
 
-        edge_event_type type;
+	eventType type;
 
 public:
-        Edge(Vertex _v1 , Vertex _v2, direction _dir = BI_DIRECTIONAL, int _speed = 50, double _length = 0, edge_event_type _type = OPEN):
-        v1(_v1),v2(_v2),dir(_dir),speed(_speed),length(_length),type(_type){}
+	Edge(string _v1, string _v2, dirType _dir = BI_DIRECTIONAL, int _speed = 30, double _length = 10, eventType _type = OPEN) :
+		v1(_v1), v2(_v2), dir(_dir), speed(_speed), length(_length), type(_type) {}
 
-        ~Edge(){}
+	~Edge() {}
 
-        Edge(const Edge& p)  // copy constructor
-        {
-            *this = p;
-        }
+	//Edge(const Edge& p)  // copy constructor
+	//{
+	//	*this = p;
+	//}
 
-        Edge & operator = (const Edge& p)    //operator =
-        {
-            if(this == &p) return(*this);
-            v1 = p.v1;
-            v2 = p.v2;
-            dir = p.dir;
-            speed = p.speed;
-            length = p.length;
-            type = p.type;
-            return (*this);
-        }
+	//Edge & operator = (const Edge& p)    //operator =
+	//{
+	//	if (this == &p) return(*this);
+	//	v1 = p.v1;
+	//	v2 = p.v2;
+	//	dir = p.dir;
+	//	speed = p.speed;
+	//	length = p.length;
+	//	type = p.type;
+	//	return (*this);
+	//}
 
-        void setEventType (edge_event_type _type)
-        {
-           type = _type;
-        }
+	void setEventType(eventType _type)
+	{
+		type = _type;
+	}
 
-        void setSpeed(int _speed)
-        {
-            speed = _speed;
-        }
+	void setSpeed(int _speed)
+	{
+		speed = _speed;
+	}
 
-        Vertex getV1(void)
-        {
-            return v1;
-        }
+	string getV1(void)
+	{
+		return v1;
+	}
 
-        Vertex getV2(void)
-        {
-            return v2;
-        }
+	string getV2(void)
+	{
+		return v2;
+	}
 
-        direction getDirection(void)
-        {
-            return dir;
-        }
+	dirType getDirection(void)
+	{
+		return dir;
+	}
 
-        int getSpeed(void)
-        {
-            return speed;
-        }
+	int getSpeed(void)
+	{
+		return speed;
+	}
 
-        double getLength(void)
-        {
-            return length;
-        }
+	double getLength(void)
+	{
+		return length;
+	}
 
-        edge_event_type getType(void)
-        {
-            return type;
-        }
+	eventType getType(void)
+	{
+		return type;
+	}
 
+	void print() {
+		cout << "edge: v1 = " << v1 << ", v2 = " << v2 << ", dir = " << dir << ", speed = " << speed << ", length = " << length << ", eventType = " << type << endl;
+	}
 };
 
 
 class Path {
-	
+
 private:
 	vector<Edge> path;
 
@@ -145,10 +161,10 @@ public:
 	Path() {}
 
 	Path(Edge edges[]) {
-			int len = sizeof(edges) / sizeof(Edge);
-			for (int i = 0; i < len; i++) {
-				path.push_back(edges[i]);
-			}
+		int len = sizeof(edges) / sizeof(Edge);
+		for (int i = 0; i < len; i++) {
+			path.push_back(edges[i]);
+		}
 	}
 
 	~Path() {}

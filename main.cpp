f6b4e76341ba4c20208e29e99dd5758a08e6b327
yannick <yannickzj@gmail.com>
2016-11-16 20:16:50 -0500

    modify file directory

--- main.cpp
+++ main.cpp
@@ -8,11 +8,11 @@ using namespace std;
 int main(int argc, char** argv) {
 
 	Graph m1;
-	m1.retrieve("test2.txt");
+	m1.retrieve("./test cases/test2.txt");
 
 	m1.trip("CLV", "340", "path1", SHORTEST);
 	m1.trip("CLV", "340", "path1", FASTEST);
-	m1.store("output.txt");
+	m1.store("output1.txt");
 
 	return 0;
 }

30c9f525f926f4d91f361e21e08388fd9c2b8618
yannick <yannickzj@gmail.com>
2016-11-19 12:16:32 -0500

    add pdf

--- main.cpp
+++ main.cpp
@@ -10,10 +10,10 @@ int main(int argc, char** argv) {
 	Graph m1;
 	m1.retrieve("./test cases/test1.txt");
 	m1.vertex("ccjil");
-	m1.edgeEvent("E1",CLOSE);
-	m1.trip("CLV", "CLN", "path1", SHORTEST);
-	m1.edgeEvent("E1",OPEN);
-	m1.trip("CLV", "CLN", "path1", FASTEST);
+	m1.edgeEvent("E26",CLOSE);
+	m1.trip("CLV", "340", "path1", SHORTEST);
+	m1.edgeEvent("E26",OPEN);
+	m1.trip("CLV", "340", "path1", FASTEST);
 	m1.addVertex("RCH",POINT_OF_INTEREST,4,5);
 	m1.addEdge("additional","RCH","CLV",V1_TO_V2,50,2,OPEN);
 	m1.vertex("CLN");
@@ -21,8 +21,8 @@ int main(int argc, char** argv) {
 
 	Graph m2;
 	m2.retrieve("./test cases/test1.txt");
-	m2.trip("CLV", "340", "path1", SHORTEST);
-	m2.trip("CLV", "340", "path1", FASTEST);
+	m2.trip("CLV", "375", "path1", SHORTEST);
+	m2.trip("CLV", "375", "path1", FASTEST);
 	vector<string> *a = new vector<string> ;
 	m2.road("test_road",*a);
 	m2.store("output2.txt");
